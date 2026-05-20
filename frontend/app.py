f92e5a3f0805ea06b7a8b79156a7ad2c692f32d8
Anugrah N M <anugrah2011@gmail.com>
2026-05-20 03:07:43 +0100

    chore: File restructure

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 uploaded_file = st.file_uploader("Choose a file")
 
-api_url = "http://127.0.0.1:8000/summarize/"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000/summarize/")
 
 print(type(uploaded_file))
 if uploaded_file is not None:
@@ -11,7 +15,7 @@ if uploaded_file is not None:
         st.toast("File Uploaded!", icon="✅")
         with st.spinner("Summarizing...", show_time=True):
             files={"file":(uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)}
-            response = requests.post(api_url, files=files)
+            response = requests.post(API_URL, files=files)
             try:
                 st.success("Done!")
                 st.markdown(response.json())
